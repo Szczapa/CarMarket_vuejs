9da8ba63b20a563ed841fa6a3d7a6754bc19a56d
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-02-06 13:58:56 +0100

    ajustement et ajout dashboard

--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -4,7 +4,7 @@
     <div v-show="showSide" class="w-[400px] h-full bg-gray-200 text-white">
       <div class="h-[50px] bg-gray-900 flex justify-start  items-center ">
         <div class="px-[20px]">
-          <h3 class="font-bold text-xl">Car Market Admin</h3>
+          <h3 class="font-bold text-xl"> <router-link to="/">Car Market Admin</router-link></h3>
         </div>
       </div>
       <div class="h-[calc(100vh-50px)] bg-gray-800 py-[20px]">
@@ -29,21 +29,21 @@
                 <path
                     d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
               </svg>
-              Profile
+              Utilisateurs
             </router-link>
           </div>
           <div class="h-[50px]">
             <div>
               <router-link
                   class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800  transition duration-400 ease-in-out"
-                  to="/">
+                  to="/me">
                 <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor"
                      viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path clip-rule="evenodd"
                         d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
                         fill-rule="evenodd"></path>
                 </svg>
-                Shop
+                About me
               </router-link>
             </div>
           </div>
