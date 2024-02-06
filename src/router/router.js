c4bca20be22b5994c3527b27f742d65a488c4f1d
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-02-06 13:58:07 +0100

    ajout et correction router

--- src/router/router.js
+++ src/router/router.js
@@ -7,20 +7,31 @@ const routes = [
         path: '/',
         component: () => import('../view/Dashboard.vue'),
         children: [
+            {name: 'home', path: '/', component: () => import('../view/Home.vue')},
             {name: 'products', path: '/products', component: () => import('../view/Products.vue')},
-            {name: 'product by id', path: '/product/:id', component: () => import('../view/Product.vue')},
+            {name: 'productById', path: '/product/:id', component: () => import('../view/Product.vue')},
             {name: 'login', path: '/login', component: () => import('../components/Login.vue')},
-            {name: 'product by id', path: '/product/:id', component: () => import('../view/Product.vue')},
+            {name: 'allUsers', path: '/users', component: () => import('../view/Users.vue')},
+            {name: 'userById', path: '/user/:id', component: () => import('../view/User.vue')},
+            {name: 'profile', path: '/profile', component: () => import('../view/Profile.vue')},
+            {name: 'aboutMe', path: '/me', component: () => import('../components/Me.vue')},
         ]
     },
-    // { name: 'products' ,path: '/products', component: () => import('../view/Products.vue') },
-    // { name: 'product by id' ,path: '/product/:id', component: () => import('../view/Product.vue') },
-    {name: 'all user', path: '/users', component: () => import('../view/Users.vue')},
-    {name: 'user by id', path: '/user/:id', component: () => import('../view/User.vue')},
-    // { name: 'create user' ,path: '/user-create', component: () => import('../components/UserCreate.vue') },
-
-    // { name: 'home' ,path: '/home', component: () => import('../view/Home.vue')},
-    {name: 'profile', path: '/profile', component: () => import('../view/Profile.vue')},
+    {
+        name: 'notFound',
+        path: '/:pathMatch(.*)*',
+        component: () => import('../view/NotFound.vue')
+    },
+    // {
+    //     name: 'serverError',
+    //     path: '/500',
+    //     component: () => import('../view/ServerError.vue')
+    // },
+    // {
+    //     name: 'shop',
+    //     path: '/shop',
+    //     component: () => import('../view/Shop.vue')
+    // },
 ]
 
 const router = createRouter({
