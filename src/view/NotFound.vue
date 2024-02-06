98ddfe08616bc696294dac99d945225760665d56
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-02-06 13:57:46 +0100

    404

---
+++ src/view/NotFound.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-400">
+    <div class="mx-auto max-w-screen-2Xl p-4 md:p-6 2xl:p-10">
+      <div class="grid grid-cols-2 gap-4">
+        <div class="flex flex-col justify-center">
+          <h2 class="font-bold"> ERREUR 404</h2>
+          <p> La page que vous cherchez est introuvable.</p>
+          <p> Notre technicien travaille dessus. En attendant, retournez à l'accueil. </p>
+          <router-link to="/"><p class="text-blue-700">Retour à l'accueil</p></router-link>
+        </div>
+        <div class="px-7.5 py-6">
+          <img class="rounded" src="../assets/ferret.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
